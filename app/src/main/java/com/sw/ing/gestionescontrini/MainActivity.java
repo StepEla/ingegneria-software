1fb1ec38c50c03ce724da28cd6520cc19f99ecd2
federicotaschin <32360377+federicotaschin@users.noreply.github.com>
2017-10-12 20:33:50 +0200

    Corretti errori gradle e template Inserito bottone per scattare foto e metodo che apre la fotocamera

--- app/src/main/java/com/sw/ing/gestionescontrini/MainActivity.java
+++ app/src/main/java/com/sw/ing/gestionescontrini/MainActivity.java
@@ -1,13 +1,25 @@
 package com.sw.ing.gestionescontrini;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    static int PHOTO_NUMBER = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void takePicture(View v){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(takePictureIntent,PHOTO_NUMBER);
+        }
     }
 }
