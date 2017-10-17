9eef337501b74bf124b66bf8a136e51875b0b05c
marcolivieri <marcolivieri@users.noreply.github.com>
2017-10-17 17:40:42 +0200

    Creata nuova Activity per la visualizzazione dello scontrino

---
+++ app/src/main/java/com/sw/ing/gestionescontrini/ViewTicketActivity.java
@@ -0,0 +1,19 @@
+package com.sw.ing.gestionescontrini;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Activity per visualizzare il singolo scontrino
+ *
+ * Created by marco.olivieri on 17/10/2017.
+ */
+
+public class ViewTicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_ticket);
+    }
+}
