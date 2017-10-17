2660b44da14142242a94181c5ec557f98dd96bed
StepEla <32414161+StepEla@users.noreply.github.com>
2017-10-17 21:48:26 +0200

    Notepad base Android, quasi completo

--- app/src/main/java/com/sw/ing/gestionescontrini/NoteSelect.java
+++ app/src/main/java/com/sw/ing/gestionescontrini/NoteSelect.java
@@ -4,11 +4,25 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NoteSelect extends AppCompatActivity {
 
+    private List<NotesBuilder> notesList = new ArrayList<>();
+    private NotesAdapter nAdapter;
+    private RecyclerView notesRecycler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +38,50 @@ public class NoteSelect extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        notesRecycler = (RecyclerView) findViewById(R.id.notes);
+
+        nAdapter = new NotesAdapter(notesList);
+        RecyclerView.LayoutManager mLayoutManager =
+                new LinearLayoutManager(getApplicationContext());
+        notesRecycler.setLayoutManager(mLayoutManager);
+        notesRecycler.setItemAnimator(new DefaultItemAnimator());
+        notesRecycler.setAdapter(nAdapter);
+
+        prepareNotes();
     }
 
+    private void prepareNotes() {
+        File directory;
+        directory = getFilesDir();
+        File[] files = directory.listFiles();
+        String theFile;
+        for (int f = 1; f <= files.length; f++) {
+            theFile = "Note" + f + ".txt";
+            NotesBuilder note = new NotesBuilder(theFile, Open(theFile));
+            notesList.add(note);
+        }
+
+    }
+    public String Open(String fileName) {
+        String content = "";
+        try {
+            InputStream in = openFileInput(fileName);
+            if ( in != null) {
+                InputStreamReader tmp = new InputStreamReader( in );
+                BufferedReader reader = new BufferedReader(tmp);
+                String str;
+                StringBuilder buf = new StringBuilder();
+                while ((str = reader.readLine()) != null) {
+                    buf.append(str + "\n");
+                } in .close();
+
+                content = buf.toString();
+            }
+        } catch (java.io.FileNotFoundException e) {} catch (Throwable t) {
+            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+
+        return content;
+    }
 }
