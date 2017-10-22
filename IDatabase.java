32ec9dda1c74d6d77085022a7d40a2f7780536ed
federicotaschin <32360377+federicotaschin@users.noreply.github.com>
2017-10-22 11:35:13 +0200

    Aggiunti commenti, @param e @return e modificato nome interfaccia

--- IDatabase.java
+++ IDatabase.java
@@ -2,24 +2,43 @@ import java.io.Serializable;
 
 /**
  * Interfaccia comune per utilizzare il database relativo agli scontrini.
+ * Questa interfaccia definisce i metodi che la classe di gestione dei dati dovrà avere.  
  */
-public interface IPhotoItem extends Serializable {
+public interface IDatabase {
 	
-	//aggiunge scontrino al db
-	//true se aggiunto correttamente
-	boolean addTicket(IPhotoItem ticket);
+	/* Aggiunge uno scontrino nel database. Si deve occupare anche della gestione del salvataggio della foto. 
+	 * L'implementazione del salvataggio della foto (file o database) è lasciata a discrezione. 
+	 * @param ticket IPhotoItem oggetto da inserire nel database, non può essere null
+	 * (Da definire quali campi dell'oggetto possono essere null e quali no)
+	 * @return boolean, true se l'inserimento è andato a buon fine, false altrimenti
+	*/
+	public boolean addTicket(IPhotoItem ticket);
 	
-	//elimina scontrino dal db
-	boolean delateTicket(IPhotoItem ticket);
+	/* Elimina uno scontrino dal database
+	 * Se la foto non è salvata nel database, questo metodo si deve occupare della sua eliminazione 
+	 * @param ticket IPhotoItem oggetto da eliminare dal database. L'oggetto può essere null, il campo  che identifica univocamente
+	 * l'entità nel database non può essere null
+	 * @return boolean, true se la rimozione è andata a buon fine, false altrimenti (es: l'oggetto non era presente nel database)
+	*/
+	public boolean deleteTicket(IPhotoItem ticket);
 	
-	//IMPORTANTE !!
-	//bisogna far aggiungere alla interfaccia del gruppo 2 la gestione del ID (in particolare set e get)
-	//restituisce uno scontrino tramite id
-	IPhotoItem getTicket(int ID);
+	/* Ritorna i dati di uno scontrino in base al suo identificatore
+	 * !!Da decidere insieme quale deve essere l'identificatore di uno scontrino!!
+ 	 * Se si sceglie di utilizzare un ID, bisogna aggiungere all'interfaccia del gruppo 2 i metodi set e get di questo
+	 * @param ID int (al momento proponiamo l'utilizzo di un identificatore di tipo INTEGER) l'ID dello scontrino a cui si vuole accedere 
+	 * @return IPhotoItem oggetto contenente i dati dello sconntrino desiderato, restituisce null se non esiste 
+	*/
+	public IPhotoItem getTicket(int ID);
 	
-	//restituisce tutti gli scontrini da db
+	/* Restituisce la lista degli scontrini presenti nel database
+	 * @return List<IPhotoItem> oggetto List contenente tutti gli oggetti IPhotoItem corrispondenti agli scontrini nel database
+	 * caso limite: restituisce una lista vuota (di dimensione zero, non un oggetto nullo!) nel caso i cui il database sia vuoto
+	*/
 	List<IPhotoItem> getAllTickets();
 	
-	//verifica se esiste già
+	/* Verifica se uno scontrino è già presente nel database
+	 * @param IPhotoItem ticket oggetto di cui verificare la presenza nel database in base all'identificatore
+	 * @return true se esiste già, false altrimenti
+	*/
 	boolean isTicket(IPhotoItem ticket);
 }
